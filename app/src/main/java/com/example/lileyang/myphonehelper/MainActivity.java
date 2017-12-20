8a6ffb480490d9e13ea01f71ec81cd2acb8fcf75
lileyang <lileyang2000@gmail.com>
2017-12-20 23:24:03 +0800

    1221 commit

--- app/src/main/java/com/example/lileyang/myphonehelper/MainActivity.java
+++ app/src/main/java/com/example/lileyang/myphonehelper/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.lileyang.myphonehelper;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    @BindView(R.id.tool_bar)
+    Toolbar mToolBar;
+
+    private View headView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+      /*  mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });*/
+
+
+        headView = mNavigationView.getHeaderView(0);
+        headView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "headerView clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.menu_app_update:
+                        Toast.makeText(MainActivity.this,"update",Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_message:
+                        Toast.makeText(MainActivity.this,"message",Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+
+                return false;
+            }
+        });
+        mToolBar.inflateMenu(R.menu.toolbar_menu);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolBar, R.string.open, R.string.close);
+        drawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(drawerToggle);
+
     }
 }
 
